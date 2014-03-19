e642ad8e2d502acafa48ef2fb8c0a2af0a98ee39
Beta <spam@harlanconnor.com>
2014-03-19 16:29:49 +0000

    first update

---
+++ Core/OmniProgram 4.1/OmniProgram 4.1/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+open System
+open System.IO
+let readLines filePath:string = 
+    let FileStream = new IO.FileStream(filePath,FileMode.Open)
+    let buffer = []
+    FileStream.BeginRead
+    "bob"
+let CopyrightNotice = "OmniProgram 4.1.0 Build © Harlan Connor"
+let BuildInc =
+    let curr = readLines("1.txt") |> ignore
+    0
+[<EntryPoint>]
+let main argv = 
+    BuildInc
+
+
+
