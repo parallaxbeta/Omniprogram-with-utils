61040dab34cc0c89a3894a2c9ac96a7362b6a311
Harlan Connor <spam@harlanconnor.com>
2014-06-09 20:02:53 +0100

    enhanced user

--- Core/OmniProgram 4.1/OmniProgram 4.1/GE.fsx
+++ Core/OmniProgram 4.1/OmniProgram 4.1/GE.fsx
@@ -1,38 +1,47 @@
-﻿module Omniprogram.GamingEngine
-open System
-open System.Collections.Generic
-type coordinates (x : float, ?y : float, ?z : float) = 
-    member val x = x with get, set
-    member val y = y with get, set
-    member val z = z with get, set
-    member x.location = (x,y,z)
-type IObject =
-     abstract member init : unit -> unit
-     abstract member name : string
-     abstract member location : coordinates
-type IMoveable =
-    abstract member move : int -> coordinates -> unit
-[<AbstractClass>]
-type Destroyable (thing : unit) = 
-     abstract member health : int with get,set
-     abstract member damage : int with get,set
-     abstract member shield : int with get,set
-     abstract member destroy : unit
-     member x.hurt (amount : int) =
-        for i in [amount .. -1 .. 1] do
-            if x.shield > 0 then
-                 x.shield <- x.shield - 1
-            else
-                x.health <- x.health - 1 
-
-type ship (name : string, spawnLocation : coordinates) = class
-    interface IObject with
-        member x.name = name
-        member x.location = spawnLocation
-        member x.init() = ()
-end
-let index = new Dictionary<string,ship>()
-let addShip (input : ship) = 
-    let x = input :> IObject
-    index.Add (x.name, input)
-
+﻿namespace Omniprogram
+module GamingEngine =
+    open System
+    open  System.Text
+    open System.Collections.Generic
+    open System.Threading
+    open System.IO
+    let diagStream = new IO.MemoryStream ()
+    let inline (!?+) (input : string) = 
+        let bytes = System.Text.Encoding.Unicode.GetBytes input
+        for i in bytes do
+            diagStream.WriteByte i
+    type coordinates (x : float, ?y : float, ?z : float) = 
+        member val x = x with get, set
+        member val y = y with get, set
+        member val z = z with get, set
+        member x.location = (x,y,z)
+    type IObject =
+         abstract member name : string
+         abstract member location : coordinates
+    type IMoveable =
+        abstract member move : int -> coordinates -> unit
+    [<AbstractClass>]
+    type Destroyable (thing : unit) = 
+         abstract member health : int with get,set
+         abstract member damage : int with get,set
+         abstract member shield : int with get,set
+         abstract member destroy : unit
+         member x.hurt (amount : int) =
+            for i in [amount .. -1 .. 1] do
+                if x.shield > 0 then
+                     x.shield <- x.shield - 1
+                else
+                    x.health <- x.health - 1 
+    
+    type ship (name : string, spawnLocation : coordinates) = class
+        do !?+ "Loading new ship"
+        interface IObject with
+            member x.name = name
+            member x.location = spawnLocation
+    end
+    let index = new Dictionary<string,ship>()
+    let addShip (input : ship) = 
+        let x = input :> IObject
+        index.Add (x.name, input)
+    
+    diagStream.ReadByte()
--- Core/OmniProgram 4.1/OmniProgram 4.1/Omniprogram.fsx
+++ Core/OmniProgram 4.1/OmniProgram 4.1/Omniprogram.fsx
@@ -1,37 +1,99 @@
 ﻿namespace Omniprogram
 open System
+module omniAddOn = 
+    type IOAO =
+        abstract member name : string
+        abstract member location : string
+module Logs = 
+    type IStream =
+        abstract member write : string -> unit
+        ///retrieves the data at the specified line number (starting at one)
+        abstract member readln : int -> string
+        abstract member data : string list
+    type ILog =
+        inherit IStream
+        abstract member lastLog : unit -> string
+        abstract member length : int
+    type GeneralLog() = class
+        interface ILog with
+            member x.data : string list = []
+            member x.write (input) = input :: (x :> ILog).data |> ignore
+            member x.readln (lineNo) = (x :> ILog).data.[lineNo-1]
+            member x.lastLog () = (x :> ILog).data.[(x :> ILog).data.Length-1]
+            member x.length = (x :> ILog).data.Length-1
+    end
+    let private secretLog = new GeneralLog()
+    let DiagLog = secretLog :> ILog
 module UID = 
-    let mutable private x:uint64 = uint64 -1
-    let getNextUID =
+    let mutable private x:uint64 = uint64 0
+    let getNextUID () =
         x <- x+ uint64 1
-        x
+        x - 1uL
+module UI = 
+    ///Writes a string, then reads in the rest of the line, which is typed in by the user
+    let prompt input=
+        printf "%s" input
+        Console.ReadLine()
+    ///Writes a line of a string, then reads in the following user-written line
+    let ask input = 
+        printfn "%s" input
+        Console.ReadLine()
 open System.Collections.Generic
-module user = 
-    type UserLevel =  | Sys | Admin | ModuleWorker | SubAdmin | BasicUser | BackgroundWorker | None
+module User = 
+    open System.Threading
+    type UserLevel =  | Sys | Admin | ModuleWorker | BasicUser | BackgroundWorker | Undefined
     ///An abstract class which is the basis for all users
     [<AbstractClass>]
-    type public user (username : string, password : string) = class
-        member private x.level : UserLevel = UserLevel.None
-        member private x.password = password
-        member public x.username = username
-        member public x.hashID = x.password.GetHashCode() + x.username.GetHashCode()
-        member public x.changePassword (newPassword:string) = x.password = newPassword   
-        member public x.passwordVerify (input : int) = if input = password.GetHashCode() then true else false
+    type user (username : string, password : string) = class
         member x.ID = UID.getNextUID
-        member val isInit = false with get, set
-            
+        member x.level : UserLevel = Undefined
+        member private x.password = password
+        member x.username = username
+        member x.hashID = x.password.GetHashCode() + x.username.GetHashCode()
+        member x.changePassword (newPassword:string) = x.password = newPassword   
+        member x.passwordVerify (input : int) = if input = password.GetHashCode() then true else false
     end
+    ///The non-creatable System Worker class
     type private Sys () = class inherit user ("","") end
-    type private Admin (username : string, password : string) = class
+    type private Admin (username, password) = class
         inherit user (username, password)
-        member private x.level = UserLevel.BasicUser
+        member x.level = UserLevel.Sys
     end
-    //type private ModuleWorker (username : sting, password : string, from : util) = class inherits  end
-    type private Basic (username : string, password, string) = class
+    ///The OAO worker class
+    type private ModuleWorker (username, password, owner : omniAddOn.IOAO) = class
+         inherit user (username, password)
+         member x.owner = owner
+         member x.level = UserLevel.ModuleWorker
+     end
+    type private Basic (username, password) = class
         inherit user (username, password)
-        member private x.level = UserLevel.BasicUser
+        member x.level = UserLevel.BasicUser
     end
-
+    type private BackgroundWorker (username : string, password) = class
+        inherit user (username, password)
+        member x.level = UserLevel.BackgroundWorker
+    end
     let allUsers = new Dictionary<uint64, user>()
-    let addUser (input : user) = 
-        allUsers.Add user.ID
+    let public addUser (usrname, pswd, level, init : user -> user, OAO : omniAddOn.IOAO option) = 
+        let newObject : user = match level with
+            |Admin -> new Admin(usrname, pswd) :> user
+            |BackgroundWorker -> new BackgroundWorker(usrname,pswd) :> user
+            |BasicUser -> new Basic(usrname,pswd) :> user
+            |ModuleWorker -> new ModuleWorker (usrname,pswd,OAO.Value) :> user
+        let x = new Threading.ParameterizedThreadStart(fun input -> ())
+        let y = new Thread(x)
+        y.Name <- usrname
+        y.Start(newObject)
+        let var = newObject.ID()
+        allUsers.Add (var, newObject)
+        y
+module Encription = 
+    let encript (data : string, key : string) = Security.Cryptography.RSA.Create(data).EncryptValue(System.Text.Encoding.ASCII.GetBytes(key))
+    let decript (data : byte [], key : string) = Security.Cryptography.RSA.Create(System.Text.Encoding.ASCII.GetString (data)).DecryptValue(System.Text.Encoding.ASCII.GetBytes(key))
+module OEXT = 
+    let readOFSF (location : string) = 
+        let reader = new System.IO.FileStream (location, IO.FileMode.Open)
+        let buffer:byte[] = Array.empty
+        let fileLength = reader.Length
+        reader.Read(buffer, 0, int fileLength) |> ignore
+        
